ee01dc479b45ca9eaac9308f3983e9479e761f25
andygimma <andy.gimma@kiron.ngo>
2017-09-22 16:14:57 -0500

    Add test

--- spec/controllers/google_sheets_engine/dashboard_controller_spec.rb
+++ spec/controllers/google_sheets_engine/dashboard_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 module GoogleSheetsEngine
   RSpec.describe DashboardController, type: :controller do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+    routes { GoogleSheetsEngine::Engine.routes }
+    it "recognizes the index template" do
+      assert_generates '/', controller: 'google_sheets_engine/dashboard', action: 'index'
     end
   end
 end
