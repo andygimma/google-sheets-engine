17a29d3e349f255d218b1d8da9463ac37cc24133
andygimma <andy.gimma@kiron.ngo>
2017-09-22 15:59:07 -0500

    add dashboard

---
+++ app/controllers/google_sheets_engine/dashboard_controller.rb
@@ -0,0 +1,9 @@
+require_dependency "google_sheets_engine/application_controller"
+
+module GoogleSheetsEngine
+  class DashboardController < ApplicationController
+    def index
+
+    end
+  end
+end
---
+++ spec/controllers/google_sheets_engine/dashboard_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module GoogleSheetsEngine
+  RSpec.describe DashboardController, type: :controller do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+end
